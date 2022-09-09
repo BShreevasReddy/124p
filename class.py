2475f8cd2c3462bc48d1c76c9f127b09ea286676
BShreevasReddy <108817946+BShreevasReddy@users.noreply.github.com>
2022-09-09 18:21:51 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,57 @@
+from flask import Flask,jsonify, request
+
+# flask object - app
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+# GET ,POST  ,PUT , DELETE
+
+#  127.0.0.1.5000/
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+# #  127.0.0.1.5000/add-data
+@app.route("/add-data", methods=["POST"])
+def add_task():
+    if not request.json:
+        return jsonify({
+            "status":"error",
+            "message": "Please provide the data!"
+        },400)
+
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({
+        "status":"success",
+        "message": "Task added succesfully!"
+    })
+    
+# 127.0.0.1.5000/get-data
+@app.route("/get-data")
+def get_task():
+    return jsonify({
+        "data" : tasks
+    }) 
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
